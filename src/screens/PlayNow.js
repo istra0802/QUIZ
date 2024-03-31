673f326660211ed6da5c2b566af8af9b59eb99a7
Nirav-Sanepara <nrsanepara@gmail.com>
2024-04-01 02:04:43 +0530

    did toggleing

--- src/screens/PlayNow.js
+++ src/screens/PlayNow.js
@@ -24,14 +24,17 @@ export default function PlayNow() {
       <ListSection />
 
       <div className="play">
-        <div class="modal "  id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-          <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+        <div class="modal " id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
               <div class="modal-body">
-              <img alt="Get More Coins" fetchpriority="high" width="70" height="74" decoding="async" data-nimg="1" src="https://images.atmequiz.com/img/wrong_coinsBox.svg" style={{color: "transparent"}}/>
-              <h2 className="heading" style={{fontSize:"34px", color:"#ff5050" , paddingTop:"0px" }} > Oops! </h2>
-              <p className="pa"> You don't have enough coins to play this contest.</p>
-              <div><div class="bonusModal_listCheck"><button class="bonusModal_reportBtn"><img alt="Get More Coins" fetchpriority="high" width="30" height="30" decoding="async" data-nimg="1" src="https://images.atmequiz.com/img/videoIcon.svg" style={{color: "transparent"}}/>Continue</button></div><p style={{marginTop:"8px", lineHeight:"18px"}}>Click on video ad to get 100 reward coins.</p></div>
+                <img alt="Get More Coins" fetchpriority="high" width="70" height="74" decoding="async" data-nimg="1" src="https://images.atmequiz.com/img/wrong_coinsBox.svg" style={{ color: "transparent" }} />
+                <h2 className="heading" style={{ fontSize: "34px", color: "#ff5050", paddingTop: "0px" }} > Oops! </h2>
+                <p className="pa"> You don't have enough coins to play this contest.</p>
+                <div><div class="bonusModal_listCheck"><button class="bonusModal_reportBtn"><img alt="Get More Coins" fetchpriority="high" width="30" height="30" decoding="async" data-nimg="1" src="https://images.atmequiz.com/img/videoIcon.svg" style={{ color: "transparent" }} />Continue</button></div><p style={{ marginTop: "8px", lineHeight: "18px" }}>Click on video ad to get 100 reward coins.</p></div>
               </div>
             </div>
           </div>
